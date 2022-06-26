0c4ca2598465fdd8c1f0b4135612d5554de5d4e9
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-06-26 17:23:44 +0700

    add github/copilot.vim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -108,6 +108,9 @@ return packer.startup(function(use)
   use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
   -- Discord Presens
   use "andweeb/presence.nvim"
+
+
+  use "github/copilot.vim"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
